74b0a3eb084c37effc04d6351e00bb598c80f484
Luis <luisale9826@gmail.com>
2020-09-20 22:36:59 -0600

    Se inicializa repositorio

---
+++ APICamaraTurismoOrosi/src/main/java/com/camaraturismoorosi/api/ApiCamaraTurismoOrosiApplication.java
@@ -0,0 +1,13 @@
+package com.camaraturismoorosi.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCamaraTurismoOrosiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCamaraTurismoOrosiApplication.class, args);
+	}
+
+}
