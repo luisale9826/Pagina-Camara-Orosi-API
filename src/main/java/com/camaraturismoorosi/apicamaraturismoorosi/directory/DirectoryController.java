4e150dc1f0dbfcf78f7d924c669fcbe253af0b8b
Luis <luisale9826@gmail.com>
2020-12-17 22:37:05 -0600

    Arreglos

--- src/main/java/com/camaraturismoorosi/apicamaraturismoorosi/directory/DirectoryController.java
+++ src/main/java/com/camaraturismoorosi/apicamaraturismoorosi/directory/DirectoryController.java
@@ -4,15 +4,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.camaraturismoorosi.apicamaraturismoorosi.model.Company;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.camaraturismoorosi.apicamaraturismoorosi.model.Company;
 
 @RestController
 @RequestMapping("visiter/cto/directory")
@@ -35,7 +35,7 @@ public class DirectoryController {
         Map<String, Object> result = new HashMap<String, Object>();
         try {
             Company company = directoryService.getCompanyById(id);
-            result.put("Company", company);
+            result.put("company", company);
         } catch (Exception e) {
             e.printStackTrace();
             result.put("Error al buscar la compañía", e.getMessage());
